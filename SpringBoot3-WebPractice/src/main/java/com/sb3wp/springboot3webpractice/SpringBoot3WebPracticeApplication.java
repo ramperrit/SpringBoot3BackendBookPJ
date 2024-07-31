e00d730faaca3d9e48f29451653e6e428993c9d8
ramperrit <153475173+ramperrit@users.noreply.github.com>
2024-07-31 12:28:27 +0900

    Initial commit

---
+++ SpringBoot3-WebPractice/src/main/java/com/sb3wp/springboot3webpractice/SpringBoot3WebPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sb3wp.springboot3webpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3WebPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3WebPracticeApplication.class, args);
+	}
+
+}
---
+++ SpringBoot3-WebPractice/src/test/java/com/sb3wp/springboot3webpractice/SpringBoot3WebPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb3wp.springboot3webpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3WebPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
